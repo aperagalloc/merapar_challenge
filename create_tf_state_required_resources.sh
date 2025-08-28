ff509218982338dad8943a3e7d4a774e76db7bff
Andres Peragallo <andresp@mill5.com>
2025-08-28 18:09:48 -0400

    ongoing changes

--- create_tf_state_required_resources.sh
+++ create_tf_state_required_resources.sh
@@ -1,7 +1,1 @@
-aws s3 mb s3://terraform-states --region us-east-1
-
-aws dynamodb create-table \
-  --table-name terraform-locks \
-  --attribute-definitions AttributeName=LockID,AttributeType=S \
-  --key-schema AttributeName=LockID,KeyType=HASH \
-  --billing-mode PAY_PER_REQUEST
+aws s3 mb s3://merapar-terraform-states --region us-east-1
--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-states"
-    key            = "merapar/code-challenge/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-locks"
-    encrypt        = true
+    bucket       = "merapar-terraform-states"
+    key          = "merapar/code-challenge/terraform.tfstate"
+    region       = "us-east-1"
+    use_lockfile = true
   }
 }
